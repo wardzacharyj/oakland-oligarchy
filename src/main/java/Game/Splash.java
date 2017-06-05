a598f34741d46d6e2c3de1a9153713b9d8c454ce
Stephen <sdowhy@gmail.com>
2017-06-05 08:15:37 -0400

    Added dynamic textbox creation for user names

--- src/main/java/Game/Splash.java
+++ src/main/java/Game/Splash.java
@@ -1,17 +1,31 @@
 package Game;
 
+import Utilities.SpringUtilities;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
-public class Splash extends JPanel implements ItemListener {
+public class Splash extends JPanel implements ItemListener, ActionListener {
 
-    private JPanel cards;
+    private JPanel topPane;
+    private JPanel middlePane;
+    private JPanel bottomPane;
 
     private Splash() {
-        super(new GridLayout(2, 1));
-        String[] numPlayersList = {"2", "3", "4"};
+        super(new GridLayout(3, 1));
+
+        add(setupTopPane());
+        add(setupMiddlePane());
+        add(setupBottomPane());
+
+    }
+
+    private JPanel setupTopPane() {
+        String[] numPlayersList = {" ", "2", "3", "4"};
         JComboBox numPlayersBox = new JComboBox<>(numPlayersList);
 
         JLabel numPlayersLabel = new JLabel("Number of Players", SwingConstants.CENTER);
@@ -24,7 +38,7 @@ public class Splash extends JPanel implements ItemListener {
         JPanel numPlayersPane = new JPanel();
         layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, numPlayersLabel, 0,
                 SpringLayout.HORIZONTAL_CENTER, numPlayersPane);
-        layout.putConstraint(SpringLayout.NORTH, numPlayersLabel, 80,
+        layout.putConstraint(SpringLayout.NORTH, numPlayersLabel, 40,
                 SpringLayout.NORTH, numPlayersPane);
 
         layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, numPlayersBox, 0,
@@ -37,26 +51,71 @@ public class Splash extends JPanel implements ItemListener {
         numPlayersPane.add(numPlayersLabel);
         numPlayersPane.add(numPlayersBox);
 
+        topPane = numPlayersPane;
+        return numPlayersPane;
 
-        add(numPlayersPane);
+    }
 
+    private JPanel setupMiddlePane() {
+        JPanel middlePanel = new JPanel();
+        middlePane = middlePanel;
+
+        return middlePanel;
+    }
+
+    private JPanel setupBottomPane() {
+        JButton startButton = new JButton("Start");
+        startButton.addActionListener(this);
+
+        SpringLayout layout = new SpringLayout();
+        JPanel bottomPanel = new JPanel();
+
+        layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, startButton, 0,
+                SpringLayout.HORIZONTAL_CENTER, bottomPanel);
+        layout.putConstraint(SpringLayout.NORTH, startButton, 0,
+                SpringLayout.NORTH, bottomPanel);
+
+        bottomPanel.setLayout(layout);
+
+        bottomPanel.add(startButton);
+
+        bottomPane = bottomPanel;
+        return bottomPanel;
     }
 
     @Override
     public void itemStateChanged(ItemEvent e) {
         if (e.getStateChange() == ItemEvent.SELECTED) {
-            int numPlayers = Integer.parseInt((String) e.getItem());
-
-            switch (numPlayers) {
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-
+            int numPlayers;
+            if (e.getItem().equals(" ")) {
+                numPlayers = 0;
+            } else {
+                numPlayers = Integer.parseInt((String) e.getItem());
             }
+
+
+            middlePane.removeAll();
+
+            for (int i = 0; i < numPlayers; i++) {
+                JTextField nameInput = new JTextField();
+
+                nameInput.setPreferredSize(new Dimension(150, 20));
+                nameInput.setText("Player " + (numPlayers + 1) + " Name");
+                nameInput.setHorizontalAlignment(SwingConstants.CENTER);
+
+                middlePane.add(nameInput);
+            }
+
+            SpringUtilities.makeGrid(middlePane, 1, numPlayers, SwingConstants.CENTER,
+                    SwingConstants.CENTER, 3, 3);
+
+            middlePane.updateUI();
         }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 
     static void splashInit() {
